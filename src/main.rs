2e1cdc8f6a858ad1a8c32500a2352a5992e73fd6
fognil <hi@fognil.com>
2026-02-27 03:35:40 +0700

    feat: set app window icon from talenode.png

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,25 @@ mod validation;
 
 use app::TaleNodeApp;
 
+fn load_icon() -> Option<egui::IconData> {
+    let png_bytes = include_bytes!("../talenode.png");
+    let img = image::load_from_memory(png_bytes).ok()?.into_rgba8();
+    Some(egui::IconData {
+        width: img.width(),
+        height: img.height(),
+        rgba: img.into_raw(),
+    })
+}
+
 fn main() -> eframe::Result {
+    let mut viewport = egui::ViewportBuilder::default()
+        .with_inner_size([1280.0, 720.0])
+        .with_min_inner_size([800.0, 500.0]);
+    if let Some(icon) = load_icon() {
+        viewport = viewport.with_icon(std::sync::Arc::new(icon));
+    }
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1280.0, 720.0])
-            .with_min_inner_size([800.0, 500.0]),
+        viewport,
         ..Default::default()
     };
 
